d5c091c0ed03b8d40fb3a8ab73af012e73ee6ab9
Abdul Basit <abdulbasitbasit076@gmail.com>
2024-04-02 22:57:22 +0500

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 
 // Code to start the client
-const client = require('./client/my-app');
+const client = require('./client');
 
 // Code to start the server
 const server = require('./server');
