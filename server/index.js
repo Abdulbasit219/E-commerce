49d1a6c9a4b536660ee5a7da8ceac0116a6e78c1
Abdul Basit <abdulbasitbasit076@gmail.com>
2024-04-02 19:33:41 +0500

    updated

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const category = require('./routes/categoryRoute.js');
 const prodCreate = require('./routes/productRoutes.js');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 //for configuration file 
 dotenv.config();
@@ -15,6 +16,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './client/dist')));
 
 const port = process.env.PORT;
 
@@ -23,9 +25,10 @@ app.use('/api/v1/auth', routes);
 app.use('/api/v1/category', category);
 app.use('/api/v1/product', prodCreate);
 
-app.get('/', (req, res) => {
-    res.send("<h1>E-commerce Project</h1>");
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/dist/index.html'));
 })
+
 
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
